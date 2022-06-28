4bc4c019463789c8b84edacf1545760df8ae097e
Pulatoff <niyozbekpulatov@gmail.com>
2022-06-28 19:08:03 +0500

    writed test resolve function in schema

--- server/schema.js
+++ server/schema.js
@@ -1,20 +1,34 @@
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+} = require("graphql");
+
+const data = [
+  { id: "1", name: "Niyozbek", gender: "male" },
+  { id: "2", name: "Mahmud", gender: "male" },
+  { id: "3", name: "Bahora", gender: "female" },
+];
 
 const PersonType = new GraphQLObjectType({
   name: "Person",
   fields: () => ({
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
-    bestFriend: { type: GraphQLString },
+    gender: { type: GraphQLString },
   }),
 });
 
 const Query = new GraphQLObjectType({
   name: "Query",
   fields: {
-    movie: {
+    person: {
       type: PersonType,
-      args: { name: { type: GraphQLString } },
-      resolve: (parent, args) => {},
+      args: { id: { type: GraphQLID } },
+      resolve: (parent, args) => {
+        return data.find((person) => person.id == args.id);
+      },
     },
   },
 });
